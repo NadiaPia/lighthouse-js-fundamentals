42a1b7a222296f3a879def53e90c3f4dabed3e9e
NadiaPia <206i417@gmail.com>
2022-04-21 07:02:40 +0000

    practice loop

---
+++ loops.js
@@ -0,0 +1,37 @@
+/*let x = 60;
+
+while (x >= 0) {
+  if (x === 50) {
+    console.log("Orbiter transfers from ground to internal power");
+  } else if (x === 31) {
+    console.log("Ground launch sequencer is go for auto sequence start ");
+  } else if (x === 16) {
+    console.log("Activate launch pad sound suppression system");
+  } else if (x === 10) {
+    console.log("Activate main engine hydrogen burnoff system ");
+  } else if (x === 6) {
+    console.log("Main engine start ");
+  }else if (x === 0) {
+    console.log("Solid rocket booster ignition and liftoff!");
+  } else {
+    console.log(`T-${x} seconds`)
+  }
+  x = x - 1; 
+}*/
+
+/*for (var x = 9; x >= 1; x--) {
+  console.log("hello " + x);
+}*/
+
+/*let solution = 1 
+
+for (var x = 12; x >= 1; x--) {
+  solution = solution * x
+}
+console.log(solution);*/
+
+for (let x = 0; x <= 25; x++) {
+  for (let y = 0; y <= 99; y++ ){
+    console.log(x + "-" + y);
+  }
+}
