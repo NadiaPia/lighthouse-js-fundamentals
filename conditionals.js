595d8d2a5fd5ed1eecae7467840135cdc6a38ba7
NadiaPia <206i417@gmail.com>
2022-04-21 07:03:26 +0000

    practice conditionals

--- conditionals.js
+++ conditionals.js
@@ -11,7 +11,7 @@ if (cold) {
 
 console.log("Now you're ready to go outside!");*/
 
-const temperature = -12;
+/*const temperature = -12;
 
 if(temperature < 0) {
   console.log("Make sure you pick out a scarf!");
@@ -25,16 +25,20 @@ console.log("Now you're ready to go outside!");
 
 
 const isCitizen = true;
-const age = 26;
-
-if (isCitizen && age > 18) {
-  console.log("You are eligible to vote.");
-}
-
+co
 if (temperature < -40 || temperature > 40) {
   console.log("Maybe going outside isn't such a great idea…");
 }
 
 if (!raining) {
   console.log("Leave your umbrella at home!");
+}*/
+
+let num = 99;
+
+while (num >= 1) {
+  const bottleString = (n) => n === 1 ? "bottle" : "bottles";
+  console.log(`${num} ${bottleString(num)} of juice on the wall! ${num} ${bottleString(num)} of juice! Take one down, pass it around... ${num - 1} ${bottleString(num - 1)} of juice on the wall!`)
+  num = num - 1;
 }
+
